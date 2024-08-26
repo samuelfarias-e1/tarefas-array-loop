80bde9daa6f7ccbd81d984dfa3b4470db1215aba
samuel-farias <samuelcontatoe1@gmail.com>
2024-08-25 23:13:38 -0300

    comecando

---
+++ 1/index.php
@@ -0,0 +1,7 @@
+<?php
+    $alunos = array("samuel"=> "10", "Henrique"=> "10", "Bruno"=>"10", "Artur"=>"10", "Pedro"=>"10");
+
+    foreach ($alunos as $x => $y){
+        echo "$x + $Y";
+    }
+?>
